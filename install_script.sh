5a2d6e5e51940069099033e6f5b417b7cf6b13ee
Richard Oliveira <richardcorp@hotmail.com>
2024-01-04 23:22:20 -0300

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -23,65 +23,65 @@ git config --global credential.helper store
 echo -e "\e[93mPasso 5: Instalando Oh My Zsh...\e[0m"
 sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/richardoliveira/script-shell/master/install_shell.sh)"
 
-#!/bin/zsh
-
+zsh -c "
 # Clonagem de plugins do Zsh
-echo -e "\e[93mPasso 6: Clonando plugins do Zsh...\e[0m"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo -e '\e[93mPasso 6: Clonando plugins do Zsh...\e[0m'
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions \${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Download do arquivo .zshrc
-echo -e "\e[93mPasso 7: Baixando arquivo .zshrc...\e[0m"
+echo -e '\e[93mPasso 7: Baixando arquivo .zshrc...\e[0m'
 curl -L https://raw.githubusercontent.com/richardoliveira/zshrc/master/.zshrc -o ~/.zshrc
 
 # Instalação do NVM
-echo -e "\e[93mPasso 8: Instalando NVM...\e[0m"
+echo -e '\e[93mPasso 8: Instalando NVM...\e[0m'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+export NVM_DIR='$HOME/.nvm' && [ -s '$NVM_DIR/nvm.sh' ] && . '$NVM_DIR/nvm.sh' && [ -s '$NVM_DIR/bash_completion' ] && . '$NVM_DIR/bash_completion'
 nvm install --lts
 
 # Instalação do PNPM e configurações
-echo -e "\e[93mPasso 9: Instalando PNPM e configurando...\e[0m"
+echo -e '\e[93mPasso 9: Instalando PNPM e configurando...\e[0m'
 npm install -g pnpm@latest
 source ~/.zshrc
 pnpm setup;
 source ~/.zshrc
 
 # Instalação do NestJS e TypeScript
-echo -e "\e[93mPasso 10: Instalando NestJS e TypeScript...\e[0m"
+echo -e '\e[93mPasso 10: Instalando NestJS e TypeScript...\e[0m'
 pnpm install -g turbo @nestjs/cli typescript
 
 # Configuração do Docker
-echo -e "\e[93mPasso 11: Configurando Docker...\e[0m"
+echo -e '\e[93mPasso 11: Configurando Docker...\e[0m'
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update && sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y && sudo usermod -aG docker $USER
-echo -e "[boot]\ncommand = service docker start" | sudo tee /etc/wsl.conf > /dev/null
+echo -e '[boot]\ncommand = service docker start' | sudo tee /etc/wsl.conf > /dev/null
 
-# Criação do diretório "sources"
-echo -e "\e[93mPasso 12: Criando diretório \"sources\"...\e[0m"
+# Criação do diretório 'sources'
+echo -e '\e[93mPasso 12: Criando diretório \"sources\"...\e[0m'
 mkdir sources
 
-if [ -z "$1" ]; then
-    parametro="false"
+if [ -z '$1' ]; then
+    parametro='false'
 else
-    parametro="$1"
+    parametro='$1'
 fi
 
-if [ "$parametro" = "true" ]; then
+if [ '$parametro' = 'true' ]; then
     # Instalação do AWS CLI e configuração
-    echo -e "\e[93mPasso 13: Instalando AWS CLI e configurando...\e[0m"
+    echo -e '\e[93mPasso 13: Instalando AWS CLI e configurando...\e[0m'
     sudo apt install python3-pip
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'
     unzip awscliv2.zip && sudo ./aws/install && sudo rm -rf aws awscliv2.zip
     pip install git-remote-codecommit
     aws configure sso
-    echo "[default]" >> ~/.aws/credentials
-    echo "aws_access_key_id=SEU_ACCESS_KEY" >> ~/.aws/credentials
-    echo "aws_secret_access_key=SEU_SECRET_KEY" >> ~/.aws/credentials
-    echo "aws_session_token=SEU_SESSION_TOKEN" >> ~/.aws/credentials
+    echo '[default]' >> ~/.aws/credentials
+    echo 'aws_access_key_id=SEU_ACCESS_KEY' >> ~/.aws/credentials
+    echo 'aws_secret_access_key=SEU_SECRET_KEY' >> ~/.aws/credentials
+    echo 'aws_session_token=SEU_SESSION_TOKEN' >> ~/.aws/credentials
     code ~/.zshrc
 fi
 
-echo -e "\e[93mScript concluído! Feche e abra o seu terminal\e[0m"
+echo -e '\e[93mScript concluído! Feche e abra o seu terminal\e[0m'
 exit
+"
