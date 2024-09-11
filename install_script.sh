f72bb8d0e34ec6e73fbbc6c7a0dfe93c02f98c17
Richard Oliveira <richardcorp@hotmail.com>
2024-09-11 07:03:49 -0300

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -21,8 +21,6 @@ git config --global credential.helper store
 
 # Instalação do Oh My Zsh
 echo -e "\e[93mPasso 5: Instalando Oh My Zsh...\e[0m"
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/richardoliveira/script-shell/master/install_shell.sh)"
-# ZSH=~/.oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 ZSH=$HOME/.oh-my-zsh RUNZSH=no sh install.sh
 
@@ -40,7 +38,7 @@ curl -L https://raw.githubusercontent.com/richardoliveira/zshrc/master/.zshrc -o
 
 # Instalação do NVM
 echo -e '\e[93mPasso 8: Instalando NVM...\e[0m'
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 source ~/.zshrc
 nvm install --lts
 
@@ -54,7 +52,7 @@ echo -e '\e[93mPasso 10: Instalando NestJS e TypeScript...\e[0m'
 pnpm install -g turbo @nestjs/cli typescript
 
 # Configuração do .NET 8
-sudo apt-get update && sudo apt-get install -y dotnet-sdk-7.0
+sudo apt-get update && sudo apt-get install -y dotnet-sdk-8.0
 sudo apt-get update && sudo apt-get install -y aspnetcore-runtime-8.0
 sudo apt-get install -y dotnet-runtime-8.0
 
@@ -62,38 +60,6 @@ sudo apt-get install -y dotnet-runtime-8.0
 sudo apt-get update && sudo apt-get install -y dotnet-sdk-7.0
 sudo apt-get update && sudo apt-get install -y aspnetcore-runtime-7.0
 sudo apt-get install -y dotnet-runtime-7.0
-
-# Configuração do Docker
-# echo -e '\e[93mPasso 11: Configurando Docker...\e[0m'
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-# echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt-get update && sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y && sudo usermod -aG docker \$USER
-# echo -e '[boot]\ncommand = service docker start' | sudo tee /etc/wsl.conf > /dev/null
-
-# Criação do diretório 'sources'
-# echo -e '\e[93mPasso 12: Criando diretório \"sources\"...\e[0m'
-# mkdir sources
-
-if [ -z \"\$1\" ]; then
-    parametro='false'
-else
-    parametro=\"\$1\"
-fi
-
-if [ \"\$parametro\" = 'true' ]; then
-    # Instalação do AWS CLI e configuração
-    echo -e '\e[93mPasso 13: Instalando AWS CLI e configurando...\e[0m'
-    sudo apt install python3-pip
-    curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'
-    unzip awscliv2.zip && sudo ./aws/install && sudo rm -rf aws awscliv2.zip
-    pip install git-remote-codecommit
-    aws configure sso
-    echo '[default]' >> ~/.aws/credentials
-    echo 'aws_access_key_id=SEU_ACCESS_KEY' >> ~/.aws/credentials
-    echo 'aws_secret_access_key=SEU_SECRET_KEY' >> ~/.aws/credentials
-    echo 'aws_session_token=SEU_SESSION_TOKEN' >> ~/.aws/credentials
-    code ~/.zshrc
-fi
 
 echo -e '\e[93mScript concluído! Feche e abra o seu terminal\e[0m'
 "
